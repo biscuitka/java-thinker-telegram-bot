daaf712791bcc8804f0fa308d7a2d53d689a744c
biscuitka <biscuitka@ya.ru>
2024-01-12 11:44:36 +0300

    init commit

---
+++ src/main/java/com/biscuitka/thinkerBot/ThinkerBotApplication.java
@@ -0,0 +1,13 @@
+package com.biscuitka.thinkerBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThinkerBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThinkerBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/biscuitka/thinkerBot/ThinkerBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.biscuitka.thinkerBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThinkerBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
